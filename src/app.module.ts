65c042d907fa978b64c3d0d61aa791332a0a4033
DavidFS <dfloressi@unsa.edu.pe>
2023-11-29 00:20:18 -0500

    configurado la db y agregar la tabla a la db

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       port: 3306,
       username: 'root',
       password: '123456',
-      database: 'Login',
+      database: 'login',
       autoLoadEntities: true,
       synchronize: true,
     }),
